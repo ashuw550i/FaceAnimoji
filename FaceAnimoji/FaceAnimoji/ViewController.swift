276d68505e2371227bc6182f34104b0642c6d5fb
Ashutosh Dingankar <ashuw550i@gmail.com>
2019-10-02 23:01:26 -0500

    Add files via upload

---
+++ FaceAnimoji/FaceAnimoji/ViewController.swift
@@ -0,0 +1,104 @@
+//
+//  ViewController.swift
+//  FaceAnimoji
+//
+//  Created by ashutosh.dingankar on 9/30/19.
+//  Copyright © 2019 ashutosh.dingankar. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    var contentNode: SCNNode? = nil
+    //var contentNode: SCNNode? = nil
+    
+    var morphs: [SCNGeometry] = []
+    let morpher = SCNMorpher()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set ViewController as ARSCNView's delegate
+        sceneView.delegate = self
+        
+        // Show statistics such as fps and timing information
+        sceneView.showsStatistics = true
+        
+        // Create a new scene
+        let scene = SCNScene(named: "art.scnassets/facial-setup-final.scn")!
+
+        // Access scene's rootNode
+        contentNode = scene.rootNode
+
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+
+    // MARK: - ARSCNViewDelegate
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        // "Reset" to run the AR session for the first time.
+        resetTracking()
+    }
+    
+    func resetTracking() {
+        guard ARFaceTrackingConfiguration.isSupported else { return }
+        let configuration = ARFaceTrackingConfiguration()
+        configuration.isLightEstimationEnabled = true
+        sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
+    }
+    
+    func session(_ session: ARSession, didFailWithError error: Error) {
+        // Present an error message to the user
+        
+    }
+    
+    func sessionWasInterrupted(_ session: ARSession) {
+        // Inform the user that the session has been interrupted, for example, by presenting an overlay
+        
+    }
+    
+    func sessionInterruptionEnded(_ session: ARSession) {
+        // Reset tracking and/or remove existing anchors if consistent tracking is required
+        
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        guard anchor is ARFaceAnchor else { return nil }
+        return contentNode
+    }
+    
+    
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        
+        guard let faceAnchor = anchor as? ARFaceAnchor
+            else { return }
+        
+        DispatchQueue.main.async {
+            let blendShapes = faceAnchor.blendShapes
+            
+            // This will only work correctly if the shape keys are given the exact same name as the blendshape names
+            for (key, value) in blendShapes {
+                if let fValue = value as? Float{                    self.contentNode?.childNodes[0].morpher?.setWeight(CGFloat(fValue), forTargetNamed: key.rawValue)
+                }
+            }
+        }
+        
+    }
+}
---
+++ FaceAnimoji/FaceAnimojiTests/FaceAnimojiTests.swift
@@ -0,0 +1,34 @@
+//
+//  FaceAnimojiTests.swift
+//  FaceAnimojiTests
+//
+//  Created by ashutosh.dingankar on 9/30/19.
+//  Copyright © 2019 ashutosh.dingankar. All rights reserved.
+//
+
+import XCTest
+@testable import FaceAnimoji
+
+class FaceAnimojiTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
